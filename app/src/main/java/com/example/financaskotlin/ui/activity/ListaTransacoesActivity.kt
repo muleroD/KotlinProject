98c139083a4b83c5819725630a1e7960b4a3c593
Richard Mulero <richard.mulero@fcamara.com.br>
2019-02-26 11:16:45 -0300

    Incluido Activity

---
+++ app/src/main/java/com/example/financaskotlin/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.financaskotlin.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
